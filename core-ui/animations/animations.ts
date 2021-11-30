b0d1b0c6c7908044c64161ab191014a17b7e2a30
pontusntengnas <pontusnt@gmail.com>
2021-11-30 21:50:00 +0100

    do some cleanup

--- core-ui/animations/animations.ts
+++ core-ui/animations/animations.ts
@@ -1,8 +1,1 @@
 export type Animation = "fade-in-right" | "fade-in-left" | "fade-in";
-
-// const fadeIn = (duration: number = 1) =>
-//   [
-//     `animation-duration: ${duration}s`,
-//     "animation-timing-function: ease-out;",
-//     "animation-name: fadeIn;",
-//   ].join(";");
