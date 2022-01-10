0e7995d9a79673df09f439732ee75c47efe9f7a9
Pontus Schuster Tengnäs <pontusnt@gmail.com>
2022-01-10 11:20:38 +0100

    0.1.7 (#4)
    * use emoji svg favicon
    
    * change favicon emoji to apple
    
    * make name header smaller to fit mobile devices better
    
    * bump deps (react-types, typescript)

---
+++ features/header-card/header-card.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import * as React from "react";
+import { Row } from "../../core-ui/building-blocks/row/row";
+import { Space } from "../../core-ui/building-blocks/space/space";
+import { Stack } from "../../core-ui/building-blocks/stack/stack";
+import { toCssVar } from "../../core-ui/colors/colors";
+import { useScreenSize } from "../../core-ui/hooks/use-screen-size";
+import { BodyText } from "../../core-ui/text/body-text";
+import { Header } from "../../core-ui/text/header";
+import { Details, SocialLink } from "../../data-fetchers/profile/types";
+import { Contact } from "../contact/contact";
+import me from "./images/me.webp";
+
+interface Props {
+  details: Details;
+  socialLinks: Array<SocialLink>;
+}
+
+const smallScreenLimit = 600;
+
+export const HeaderCard: React.VFC<Props> = ({ details, socialLinks }) => {
+  const { width } = useScreenSize();
+  const isSmallScreen = width && width <= smallScreenLimit;
+
+  const name = `${details.name} ${details.lastName}`;
+  const subTitle = `${details.description} Based in ${details.location}.`;
+
+  return (
+    <Row backgroundColor={toCssVar("white")} flexWrap={"wrap"} centerContent>
+      <>
+        <Row animations={["fade-in-left"]}>
+          <Image
+            src={me}
+            alt={name}
+            placeholder={"blur"}
+            width={170}
+            height={170}
+            quality={100}
+          />
+        </Row>
+        <Space width={30} />
+        <Stack animations={["fade-in-right"]} centerContent>
+          {isSmallScreen && <Space height={10} />}
+          <Header text={name} size={isSmallScreen ? 2 : 1} />
+          <BodyText text={subTitle} />
+          <Contact socialLinks={socialLinks} />
+        </Stack>
+      </>
+    </Row>
+  );
+};
